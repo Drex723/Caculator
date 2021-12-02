de9e1daad7deff24121f57775d46d99ea523e290
Okunoye David <drexxdrey00@gmail.com>
2021-12-02 17:56:18 +0100

    first commit

---
+++ Event Listeners/index.js
@@ -0,0 +1,5 @@
+const grandParent = document.querySelector(".grandparent") 
+const parent = document.querySelector(".parent") 
+const child = document.querySelector(".child") 
+
+grandParent.addEventListener(('click', ))
